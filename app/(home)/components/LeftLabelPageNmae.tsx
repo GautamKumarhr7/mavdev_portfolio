45a656ebc3b244b153ab09f842bad89b9b272a7f
GautamKumarhr7 <kumargautam3231@gmail.com>
2025-03-26 00:29:06 +0530

    feat: added skill about section

--- app/(home)/components/LeftLabelPageNmae.tsx
+++ app/(home)/components/LeftLabelPageNmae.tsx
@@ -1,15 +1,15 @@
 import { PageLabelProps } from "@/app/types";
+
 function LeftLabelPageName({ pageName, label }: PageLabelProps) {
   return (
-    <>
-      <div className="hidden md:flex left-6 top-24 flex-col items-center">
-        <p className="text-2xl font-light">{label}</p>
-        <div className="w-0.5 h-40 bg-gray-600 mt-4"></div>
-        <p className="mt-6 rotate-90 text-sm text-purple-400 tracking-wide">
-          {pageName}
-        </p>
-      </div>
-    </>
+    <div className="flex left-6 top-24 flex-col items-center">
+      <p className="text-2xl font-light">{label}</p>
+      <div className="w-0.5 h-40 bg-gray-600 mt-4"></div>
+      <p className="mt-15 rotate-90 text-sm text-purple-400 tracking-wide whitespace-nowrap">
+        {pageName}
+      </p>
+    </div>
   );
 }
+
 export default LeftLabelPageName;
