5fc1e198b63b99318c3416ef1c98bde4c718ef11
GautamKumarhr7 <kumargautam3231@gmail.com>
2025-03-30 00:28:50 +0530

    init

--- app/(home)/experience/page.tsx
+++ app/(home)/experience/page.tsx
@@ -1,7 +1,12 @@
+import Navbar from "../_components/Navbar";
+import Footer from "../_components/Footer";
+
 export default function Experiance() {
   return (
     <div>
+      <Navbar />
       <h1>working on it</h1>
+      <Footer />
     </div>
   );
 }
