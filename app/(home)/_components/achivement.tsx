b6809fd5e94c3c1242a9105e982d94bd88262e78
GautamKumarhr7 <kumargautam3231@gmail.com>
2025-06-27 22:33:27 +0530

    feat: achivement page

---
+++ app/(home)/_components/achivement.tsx
@@ -0,0 +1,9 @@
+export default function Achivement() {
+  return (
+    <div className="bg-gradient-to-b from-[#0F172A] to-[#1E293B] flex flex-col min-h-screen">
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-white text-3xl font-bold">Achievements</h1>
+      </div>
+    </div>
+  );
+}
--- app/(home)/achivement/page.tsx
+++ app/(home)/achivement/page.tsx
@@ -1,7 +1,13 @@
+import Achivement from "../_components/achivement";
+import Navbar from "../_components/Navbar";
+import Footer from "../_components/Footer";
 export default function Achievement() {
   return (
     <div>
+      <Navbar />
       <h1>work in progress! stay tune</h1>
+      <Achivement />
+      <Footer />
     </div>
   );
 }
