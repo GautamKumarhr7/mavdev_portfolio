e45fcc7aca8096832845b3f89323763362708618
GautamKumarhr7 <kumargautam3231@gmail.com>
2025-07-20 22:40:23 +0530

    fix: about page height fix

--- app/(home)/about/page.tsx
+++ app/(home)/about/page.tsx
@@ -6,7 +6,7 @@ export default function AboutMe() {
   return (
     <div className="bg-gradient-to-b from-[#0F172A] to-[#1E293B] flex flex-col min-h-screen">
       <Navbar />
-      <div className="">
+      <div className="h-screen flex items-center justify-center">
         <About />
       </div>
       <div>
